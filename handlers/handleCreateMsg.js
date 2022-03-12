d9d8e3f0e09621e440bdac886c551d18f6757bc8
acessrdpgg <78134044+acessrdpgg@users.noreply.github.com>
2022-03-12 20:21:15 +0530

    Update handleCreateMsg.js

--- handlers/handleCreateMsg.js
+++ handlers/handleCreateMsg.js
@@ -159,7 +159,7 @@ const handleCreateMsg = async (msg , client , MessageMedia) => {
                     const ss = await new MessageMedia(sData.mimetype , sData.data);
                     const sendAsSticker = msg.endsWith('sticker') ? true : false;
                     for(let i = 0; i < intervals; i++) {
-                        quotedMsg.reply(ss, null , {sendMediaAsSticker:true});
+                        quotedMsg.reply(ss, null , {sendMediaAsSticker: sendAsSticker});
                      }
                 } else {
                     for(let i = 0; i < intervals; i++) {
