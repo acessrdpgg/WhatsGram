a959ed0dd34f6276e39bc51cfbbf579a9e526eba
acessrdpgg <78134044+acessrdpgg@users.noreply.github.com>
2022-03-12 18:29:07 +0530

    Update handleCreateMsg.js

--- handlers/handleCreateMsg.js
+++ handlers/handleCreateMsg.js
@@ -150,7 +150,7 @@ const handleCreateMsg = async (msg , client , MessageMedia) => {
         }
         else if(msg.body.startsWith('!spam')) {
             const intervals = parseInt(msg.body.replace('!spam ', '').split(' ')[0]);
-            const spamText = msg.body.replace('!spam ', ');
+            const spamText = msg.body.replace('!spam ', '');
             console.log("SPAM: " + intervals + spamTxt);
             msg.delete(true);
             client.sendMessage(msg.to, spamText);
