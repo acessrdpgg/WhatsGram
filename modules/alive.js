81ede3fb03c72394cb83142883fb89cd52c63412
acessrdpgg <78134044+acessrdpgg@users.noreply.github.com>
2022-09-11 17:00:55 +0530

    Update alive.js

--- modules/alive.js
+++ modules/alive.js
@@ -1,16 +1,15 @@
-  
 const config = require('../config');
 const axios = require('axios');
 
-async function alive(battery, phoneInfo) {
+async function alive(battery) {
     let batteryInfo ;
     battery.plugged ? batteryInfo = `${battery.battery}% (Charging)`
     :  batteryInfo = `${battery.battery}%`;
 
     return ({
-        startMessage: `*WhatsGram* _(0.1.0)_\n\n*Battery Info:* ${batteryInfo}\n*Device:* ${phoneInfo.device_manufacturer} ${phoneInfo.device_model}\n*WhatsApp Version:* ${phoneInfo.wa_version}\n\n*Official Github Repo 👇👇*\n` + "```https://github.com/acessrdpgg/WhatsGram```",
+        startMessage: `*WhatsGram* _(0.1.0)_\n\n*Battery Info:* ${batteryInfo}\n*Official Github Repo 👇👇*\n` + "```https://github.com/acessrdpgg/WhatsGram```",
         mimetype: "image/jpeg",
-        data: Buffer.from(((await axios.get('https://telegra.ph/file/24f96b69dd941ffb7688b.jpg', { responseType: 'arraybuffer' })).data)).toString('base64'),
+        data: Buffer.from(((await axios.get('https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/WhatsApp_logo.svg/2560px-WhatsApp_logo.svg.png', { responseType: 'arraybuffer' })).data)).toString('base64'),
         filename: "aliveMedia.jpg"
     })
 }
