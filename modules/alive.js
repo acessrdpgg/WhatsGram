08609178483ac09e30440f5839a8ffde7d3439c5
acessrdpgg <78134044+acessrdpgg@users.noreply.github.com>
2022-10-02 03:19:36 +0530

    Update alive.js

--- modules/alive.js
+++ modules/alive.js
@@ -1,13 +1,13 @@
 const config = require('../config');
 const axios = require('axios');
 
-async function alive(battery) {
+async function alive(battery, client) {
     let batteryInfo ;
     battery.plugged ? batteryInfo = `${battery.battery}% (Charging)`
     :  batteryInfo = `${battery.battery}%`;
 
     return ({
-        startMessage: `*WhatsGram* _(0.1.0)_\n\n*Battery Info:* ${batteryInfo}\n*Official Github Repo 👇👇*\n` + "```https://github.com/acessrdpgg/WhatsGram```",
+        startMessage: `*WhatsGram @BeastAvin*\n\n*Platfrom:* ${client.info.platform}\n*Battery Info:* ${battery.plugged}\n*Official Github Repo 👇👇*\n` + "```https://github.com/acessrdpgg/WhatsGram```",
         mimetype: "image/jpeg",
         data: Buffer.from(((await axios.get('https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/WhatsApp_logo.svg/2560px-WhatsApp_logo.svg.png', { responseType: 'arraybuffer' })).data)).toString('base64'),
         filename: "aliveMedia.jpg"
